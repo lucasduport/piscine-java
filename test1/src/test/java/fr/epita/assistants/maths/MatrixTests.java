0f9d9783c2ab3b422801f6a4b0c2ec01ea2c91c4
Lucas Duport <lucas.duport@epita.fr>
2024-02-05 13:52:42 +0100

    exercise-test1-1

---
+++ test1/src/test/java/fr/epita/assistants/maths/MatrixTests.java
@@ -0,0 +1,99 @@
+package fr.epita.assistants.maths;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MatrixTests {
+    @Test
+    void constructTest() {
+        int[][] val = new int[2][2];
+        Matrix m = new Matrix(val);
+        assertEquals(m.getMatrix(), val);
+        assertInstanceOf(Matrix.class, m);
+    }
+
+    @Test
+    void constructTest1() {
+        int[][] val = new int[3][3];
+        val[0][2] = 8;
+        Matrix m = new Matrix(val);
+        assertInstanceOf(Matrix.class, m);
+        assertEquals(m.getMatrix(), val);
+
+    }
+
+    @Test
+    void constructTest2() {
+        int[][] val = null;
+        Matrix m = new Matrix(val);
+        assertEquals(m.getMatrix(), val);
+    }
+
+    @Test
+    void constructTest3() {
+        int[][] val = new int[0][0];
+        Matrix m = new Matrix(val);
+        assertEquals(m.getMatrix(), val);
+        assertInstanceOf(Matrix.class, m);
+    }
+
+    @Test
+    void equalTest1() {
+        int[][] val = new int[2][2];
+        val[1][0] = 5;
+        Matrix m = new Matrix(val);
+        Matrix j = new Matrix(val);
+        assertTrue(m.equals(m));
+        assertFalse(m.equals(j));
+    }
+
+    @Test
+    void equalTest2() {
+        int[][] val = new int[2][2];
+        val[1][0] = 5;
+        Matrix m = new Matrix(val);
+        Matrix j = null;
+        assertTrue(m.equals(m));
+        assertFalse(m.equals(j));
+    }
+
+    @Test
+    void equalTest3() {
+        int[][] val = new int[2][2];
+        val[1][0] = 5;
+        Matrix m = new Matrix(val);
+        assertTrue(m.equals(m));
+        val[0][1] = 3;
+        assertFalse(m.equals(m));
+    }
+
+    @Test
+    void equalTest4() {
+        int[][] val = new int[3][2];
+        val[1][0] = 5;
+        Matrix m = new Matrix(val);
+        Matrix j = new Matrix(new int[3][7]);
+        assertFalse(m.equals(j));
+        assertFalse(j.equals(m));
+        assertEquals(m.equals(j), j.equals(m));
+    }
+
+    @Test
+    void multiplyTest1()
+    {
+        int [][] val = new int[1][1];
+        val[0][0] = 0;
+        Matrix m = new Matrix(val);
+        assertFalse(m.multiply(m).equals(m));
+    }
+
+    @Test
+    void multiplyTest2()
+    {
+        int [][] val = new int[1][1];
+        val[0][0] = 0;
+        Matrix m = new Matrix(null);
+        assertTrue(m.multiply(m).equals(m));
+    }
+}

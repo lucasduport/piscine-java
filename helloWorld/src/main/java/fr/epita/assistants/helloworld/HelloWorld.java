2d52410c92f3c95b8ebe483d81843a855f9371a9
Lucas Duport <lucas.duport@epita.fr>
2024-02-05 10:23:38 +0100

    exercise-helloWorld-

---
+++ helloWorld/src/main/java/fr/epita/assistants/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package fr.epita.assistants.helloworld;
+
+public class HelloWorld {
+    public void printHelloWorld()
+    {
+        System.out.print("Hello World!");
+    }
+
+    public void printHelloWorldErr()
+    {
+        System.err.println("Hello World!");
+    }
+}

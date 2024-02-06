21db3a5206c0c2c8b01c7a31f348ada5b36a05d4
Lucas Duport <lucas.duport@epita.fr>
2024-02-06 11:34:19 +0100

    exercise-streamStudent-2

--- streamStudent/src/main/java/fr/epita/assistants/streamstudent/Streamer.java
+++ streamStudent/src/main/java/fr/epita/assistants/streamstudent/Streamer.java
@@ -1,41 +1,67 @@
 package fr.epita.assistants.streamstudent;
 
+import javax.swing.*;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Streamer {
-    public Stream<Pair<Integer, String>> validator(Stream<Pair<Integer, String>> stream)
-    {
-        stream.filter(elt -> elt.getKey() <= 100 && elt.getKey() >= 0)
+    public Stream<Pair<Integer, String>> validator(Stream<Pair<Integer, String>> stream) {
+
+        return stream.filter(elt -> elt.getKey() <= 100 && elt.getKey() >= 0)
                 .filter(elt -> (elt.getValue().matches(".*[.]{1}.*") ||
-                        elt.getValue().matches(".*[_]{1}.*") ));
-        return stream;
+                        elt.getValue().matches(".*[_]{1}.*")));
     }
 
-    public Stream<Pair<Integer, String>> orderGrade(Stream<Pair<Integer, String>> stream)
-    {
-        stream.sorted(
+    public Stream<Pair<Integer, String>> orderGrade(Stream<Pair<Integer, String>> stream) {
+        return stream.sorted(
                 Comparator.comparing(Pair::getKey)
         );
-        return stream;
     }
 
     public Stream<Pair<Integer, String>> lowercase(Stream<Pair<Integer, String>> stream) {
-        stream.map(
+        ;
+        return stream.map(
                 elt -> new Pair<Integer, String>(elt.getKey(), elt.getValue().toLowerCase())
         );
-        return stream;
     }
 
-    public Optional<Pair<Integer, String>> headOfTheClass(Stream<Pair<Integer, String>> stream)
-    {
-        stream.sorted(
+    public Optional<Pair<Integer, String>> headOfTheClass(Stream<Pair<Integer, String>> stream) {
+        Stream<Pair<Integer, String>> newStream = stream.sorted(
                 Comparator.comparing(Pair::getKey)
         );
-        //stream.filter(
-         //       elt -> (elt.getKey() == stream.max(Comparator.comparing(Pair::getKey).get()))
-        //)
-        return stream.max(Comparator.comparing(Pair::getKey));
+        List<Pair<Integer, String>> l = newStream.toList();
+        int maxi = l.get(l.size() - 1).getKey();
+        Stream<Pair<Integer, String>> s1 = l.stream();
+        Stream<Pair<Integer, String>> s2 = s1.filter(
+                elt -> elt.getKey() == maxi
+        );
+        return s2.sorted(Comparator.comparing(Pair::getValue)).findAny();
+
+    }
+
+    public Stream<Pair<Integer, String>> quickFix(Stream<Pair<Integer, String>> stream) {
+        return stream.map(
+                elt -> {
+                    if ((elt.getValue().matches("[m|M][a|A].*") ||
+                            elt.getValue().matches("[L|l]*.*x")))
+                        return new Pair<Integer, String>(elt.getKey() * 2 % 100, elt.getValue());
+                    else
+                        return elt;
+                }
+        );
+    }
+
+    public Stream<Pair<Integer, String>> encryption(Stream<Pair<Integer, String>> stream) {
+        return stream.map(
+                elt ->
+                {
+                    return new Pair<Integer, String>(elt.getKey(), elt.getValue().substring(elt.getValue().length() / 2,
+                            elt.getValue().length() - 1) + elt.getValue().substring(0, elt.getValue().length() / 2));
+                }
+        );
     }
 }
